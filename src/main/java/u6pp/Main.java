c025a99f71fc6581f0fc0063f7fc915a81890055
Eric Hsieh <erich@villagechristian.org>
2023-02-10 19:55:28 -0800

    remove solution

--- src/main/java/u6pp/Main.java
+++ src/main/java/u6pp/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to u6pp! :)\nGood luck!");
 
-        UnoFrontend game = new UnoFrontend();
-        game.play();
+        //UnoFrontend game = new UnoFrontend();
+        //game.play();
+
+        // use this space to test your code :)
     }
 }
